673a10b46af23bb53da8d7186450f1002b625bbb
Corrine Olson <silverelizard@gmail.com>
2013-12-12 20:19:52 -0500

    Basic Search and Requests

--- BeerTech/Scripts/ngapps/request.js
+++ BeerTech/Scripts/ngapps/request.js
@@ -1,4 +1,4 @@
-﻿function RequestCtrl($scope, $http) {
+﻿function RequestCtrl($scope, $http, $filter) {
     $scope.requests = [];
     $scope.loaded = false;
     $scope.noresults = true;
@@ -24,7 +24,24 @@
             }
         }
         $scope.loaded = true;
-    }    
+    }
+
+    $scope.date = function (dt) {
+        var date = new Date(parseInt(dt.substr(6)));
+        return date.getMonth() + "/" + date.getDate() + "/" + date.getFullYear();
+    }
+
+    $scope.panelType = function (status) {
+        if(status == "Submitted") { return "panel-info"; } 
+        else if(status == "Bought") { return "panel-success"; } 
+        else if(status == "Rejected") { return "panel-danger"; }
+        else if (status == "Not Found") { return "panel-warning"; }
+        return "panel-default";
+    }
+
+    $scope.filterSearch = function () {
+        $scope.rows = chunk($filter('filter')($scope.requests, $scope.search), 3);
+    }
 }
 
 function chunk(a, s) {
@@ -32,3 +49,4 @@ function chunk(a, s) {
         (x = i % s) ? n[c][x] = a[i] : n[++c] = [a[i]];
     return n;
 }
+
